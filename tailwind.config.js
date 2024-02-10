d5ced27fb076b9fbdd7b891efb4cfb6c805bca0a
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-02-10 22:09:16 +0100

    Dark mode by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: ['class', '[data-mode="dark"]'],
 };
